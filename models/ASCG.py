84f07d6d7bda2f7581ed172131f2c7467d827aeb
PecholaL <pechola.lee@outlook.com>
2024-03-06 11:35:14 +0800

    むいか

--- models/ASCG.py
+++ models/ASCG.py
@@ -15,7 +15,8 @@ class ASCG(nn.Module):
         super().__init__()
         with open(config_path) as f:
             self.config = yaml.load(f, Loader=yaml.FullLoader)
-        self.cinn = self.build_inn()
+
+        self.cinn = self.build_inn(self.config)
         self.trainable_parameters = [
             p for p in self.cinn.parameters() if p.requires_grad
         ]
@@ -27,3 +28,38 @@ class ASCG(nn.Module):
             lr=self.config["ASCG"]["training"]["lr"],
             weight_decay=["ASCG"]["training"]["weight_decay"],
         )
+
+    def build_inn(config):
+        def subnet():
+            return nn.Sequential(
+                nn.Linear(config["ASCG"]["struct"]["ch_in"], 512),
+                nn.ReLU(),
+                nn.Linear(512, config["ASCG"]["struct"]["ch_out"]),
+            )
+
+        cond = Ff.ConditionNode(config["ASCG"]["struct"]["cond_node_size"])
+        nodes = [Ff.InputNode()]
+
+        nodes.append(Ff.Node(nodes[-1], Fm.Flatten, {}))
+        for k in range(20):
+            nodes.append(Ff.Node(nodes[-1], Fm.PermuteRandom, {"seed": k}))
+            nodes.append(
+                Ff.Node(
+                    nodes[-1],
+                    Fm.GLOWCouplingBlock,
+                    {"subnet_constructor": subnet, "clamp": 1.0},
+                    conditions=cond,
+                )
+            )
+
+        return Ff.ReversibleGraphNet(
+            nodes + [cond, Ff.OutputNode(nodes[-1])], verbose=False
+        )
+
+    def forward(self, x, cond):
+        z = self.cinn(x, cond)
+        jac = self.cinn.log_jacobian(run_forward=False)
+        return z, jac
+
+    def reverse_sample(self, z, cond):
+        return self.cinn(z, cond, rev=True)
