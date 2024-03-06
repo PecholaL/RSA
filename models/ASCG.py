87de9f9abf32237a785c44b4e7517b532ec5cf35
PecholaL <pechola.lee@outlook.com>
2024-03-06 11:26:08 +0800

    むいか

--- models/ASCG.py
+++ models/ASCG.py
@@ -7,10 +7,14 @@ import torch.nn as nn
 import FrEIA.framework as Ff
 import FrEIA.modules as Fm
 
+import yaml
+
 
 class ASCG(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, config_path) -> None:
         super().__init__()
+        with open(config_path) as f:
+            self.config = yaml.load(f, Loader=yaml.FullLoader)
         self.cinn = self.build_inn()
         self.trainable_parameters = [
             p for p in self.cinn.parameters() if p.requires_grad
@@ -19,5 +23,7 @@ class ASCG(nn.Module):
             p.data = 0.01 * torch.randn_like(p)
 
         self.optim = torch.optim.Adam(
-            self.trainable_parameters, lr=lr, weight_decay=weight_decay
+            self.trainable_parameters,
+            lr=self.config["ASCG"]["training"]["lr"],
+            weight_decay=["ASCG"]["training"]["weight_decay"],
         )
